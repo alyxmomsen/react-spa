9c7c8577f26b8c45645bf2ca4357ca9f08c3ae19
alex <shizofranky@gmail.com>
2024-03-12 21:51:02 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { useReducer, useState } from 'react';
+import { type } from '@testing-library/user-event/dist/type';
 
 
-function reducer () {
 
-  return ;
+
+function reducer ( state , action ) {
+
+  switch (action.type) {
+    case 'increment':
+      return state + action.payload ;
+    case 'decrement':
+      return state - action.payload ;
+    default :
+      return state ;
+  }
+  
 }
 
 
 function App() {
 
-  
-
-  const rh = useReducer();
+  const [state , dispatch]  = useReducer(reducer , 0);
 
   return (
     <div className="App">
-      foobar
+      <p>{state}</p>
+      <button onClick={() => {dispatch({type:'decrement' , payload:1})}}>click</button>
     </div>
   );
 }
